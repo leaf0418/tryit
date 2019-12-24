c4b9b3a455b685cf397b54f7e28c76ea69d28106
yeh yun <leaf41814@gmail.com>
2019-12-24 22:04:17 +0800

    try

--- ImageMasaic.py
+++ ImageMasaic.py
@@ -2,20 +2,30 @@ import cv2
 import numpy as np
 
 
-def do_Mosaic(filename, neighbor):
+def do_Mosaic(filename, patch):
     img = cv2.imread(filename)
     img_mosaic = img.copy()
     h, w, _ = img.shape
 
-    for r in range(0, h, neighbor):
-        for c in range(0, w, neighbor):
-            patch = img[r:r + neighbor, c:c + neighbor, :]
+    if patch == 'Big':
+        patch_size = 10
+    elif patch == 'Mid':
+        patch_size = 30
+    else:
+        patch_size = 50
+
+    neighbor_h = int(h / patch_size)
+    neighbor_w = int(w / patch_size)
+
+    for r in range(0, h, neighbor_h):
+        for c in range(0, w, neighbor_w):
+            patch = img[r:r + neighbor_h, c:c + neighbor_w, :]
             avg_patch = np.mean(np.mean(patch, axis=0), axis=0)
             avg_patch = np.expand_dims(np.expand_dims(avg_patch, 0), 0)
-            img_mosaic[r:r + neighbor, c:c + neighbor, :] = avg_patch
+            img_mosaic[r:r + neighbor_h, c:c + neighbor_w, :] = avg_patch
 
     fn = filename.rsplit('.', 1)
-    newfile = fn[0] + "_p." + ''.join(fn[1:])
+    newfile = fn[0] + "_q." + ''.join(fn[1:])
     print(newfile)
     cv2.imwrite(newfile, img_mosaic)
 
